c2817dc29923ad7d101a75864d860e8229478d48
dantain <d5361920@gmail.com>
2021-01-15 17:24:45 +0800

    no message

--- InvoicingMgmSys/IMS/src/main/java/ims/controller/LoginEntrance.java
+++ InvoicingMgmSys/IMS/src/main/java/ims/controller/LoginEntrance.java
@@ -1,11 +1,14 @@
 package ims.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import ims.entity.Program;
 import ims.entity.User;
 import ims.service.serviceImpl.LoginServiceImpl;
 
@@ -19,15 +22,16 @@ public class LoginEntrance {
 	@RequestMapping(value="byName", method=RequestMethod.POST)
 	public void loginCheck(Model model, String userName, String pwd) {
 		User userData = null;
-		
+		List<Program> programList = null;
 		// 1. 帳號密碼去DB查詢是否存在
 		if((userData = loginServiceImpl.checkAccount(userName, pwd)) != null) {
 			
 			String userId = userData.getUser_id();
 			// 2. 用取得的userData裡的userId去找對應roleId(多筆)
-			
 			// 3. 使用roleId，去找對應programId(多筆)
 			// 4. 使用programId， 去找對應program_name
+			programList = loginServiceImpl.getUseProgram(userId);
+			model.addAttribute("List", programList);
 			
 		}
 		else {
